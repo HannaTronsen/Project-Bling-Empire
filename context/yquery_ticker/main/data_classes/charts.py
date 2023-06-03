567cc12cee3d9cb8d2785c5aea6b9b12291ca792
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-06-03 14:59:12 +0200

    refactor for better readability

--- context/yquery_ticker/main/data_classes/charts.py
+++ context/yquery_ticker/main/data_classes/charts.py
@@ -1,5 +1,6 @@
 import re
 from dataclasses import dataclass
+from typing import Any
 from context.yquery_ticker.main.const import QUARTER_REGEX, QUARTER_YEAR_REGEX, YEAR_REGEX
 from context.yquery_ticker.main.enums.quarter import Quarter
 import datetime
@@ -11,38 +12,56 @@ class Date:
     quarter: Quarter = None
 
     @classmethod
-    def convert_date(self, value):
-        if value is None or value == "" or value == "N/A":
-            return None
-
-        is_valid_year_int = (
+    def is_valid_year(self, value: Any) -> bool:
+        return (
             type(value) != str
             and type(value) != float
             and value >= 0
         )
-        if (is_valid_year_int):
-            return Date(year=value)
 
-        elif (type(value) == str):
-
-            quarter_and_year = re.search(
-                QUARTER_YEAR_REGEX, value, re.IGNORECASE)
-            if quarter_and_year:
-                quarter_date = str(quarter_and_year.group(1)).upper()
-                year = int(quarter_and_year.group(2))
-                return Date(year=year, quarter=Quarter.from_quarter_date(quarter_date=quarter_date))
-
-            only_year = re.search(YEAR_REGEX, value)
-            if only_year:
-                year = int(only_year.group())
-                return Date(year=year)
-
-            only_quarter = re.search(QUARTER_REGEX, value, re.IGNORECASE)
-            if only_quarter:
-                quarter_date = str(only_quarter.group()).upper()
-                return Date(year=datetime.datetime.year, quarter=Quarter.from_quarter_date(quarter_date=quarter_date))
-
+    @classmethod
+    def is_valid_year_only(self, value: str):
+        match = re.search(YEAR_REGEX, value)
+        if (match):
+            year = int(match.group())
+            return Date(year=year)
         return None
+
+    @classmethod
+    def is_valid_quarter_only(self, value: str):
+        match = re.search(QUARTER_REGEX, value, re.IGNORECASE)
+        if (match):
+            quarter_date = str(match.group()).upper()
+            return Date(year=datetime.datetime.year, quarter=Quarter.from_quarter_date(quarter_date=quarter_date))
+        return None
+
+    @classmethod
+    def is_valid_quarter_and_year(self, value):
+        match = re.search(QUARTER_YEAR_REGEX, value, re.IGNORECASE)
+        if (match):
+            quarter_date = str(match.group(1)).upper()
+            year = int(match.group(2))
+            return Date(year=year, quarter=Quarter.from_quarter_date(quarter_date=quarter_date))
+        return None
+
+    @classmethod
+    def convert_date(self, value):
+        date = None
+        if value is not None and value != "" and value != "N/A":
+            if (self.is_valid_year(value=value)):
+                return Date(year=value)
+
+            elif (type(value) == str):
+
+                if self.is_valid_quarter_and_year(value) is not None:
+                    date = self.is_valid_quarter_and_year(value)
+                elif self.is_valid_year_only(value) is not None:
+                    date = self.is_valid_year_only(value)
+                elif self.is_valid_quarter_only(value) is not None:
+                    date = self.is_valid_quarter_only(value)
+
+        return date
+
 
 @dataclass
 class QuarterlyEarningsDataChart:
@@ -62,4 +81,3 @@ class YearlyFinancialsDataChart:
     date: Date
     revenue: float
     earnings: float
-    
