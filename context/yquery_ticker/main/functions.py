19bd4fdb6d3e5c0495c9ed626cb3c084dafc888e
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-08-30 23:02:58 +0200

    refactored functions.py

--- context/yquery_ticker/main/functions.py
+++ context/yquery_ticker/main/functions.py
@@ -1,6 +1,7 @@
 from yahooquery import Ticker
 import re
-from const import (BLACKLISTED_STOCK_TICKERS_PATH, CONST_COLLECTION,)
+from const import (BLACKLISTED_STOCK_TICKERS_PATH, CONST_COLLECTION)
+
 
 def put_yahoo_query_ticker_object(ticker):
     pass
@@ -9,7 +10,6 @@ def put_yahoo_query_ticker_object(ticker):
 def validate_yahoo_query_ticker_objects():
     blacklisted_stocks_file = open(BLACKLISTED_STOCK_TICKERS_PATH, "r+")
     blacklisted_stocks_file_content = blacklisted_stocks_file.read()
-    new_line = blacklisted_stocks_file.write("\n")
 
     for stock_collection in CONST_COLLECTION.STCOK_COLLECTION_LIST:
         stock_tickers_file = open(stock_collection.file_path, "r").readlines()
@@ -23,12 +23,12 @@ def validate_yahoo_query_ticker_objects():
                 pass
             elif not bool(yquery_ticker.summary_detail):
                 print(f"No information found for ticker '{ticker}'.")
-                blacklisted_stocks_file.write(ticker.strip() + new_line)
+                blacklisted_stocks_file.write(f"{ticker.strip()}\n")
             else:
                 put_yahoo_query_ticker_object(ticker.strip())
 
     blacklisted_stocks_file.close()
 
+
 def validate_and_get_yahoo_query_ticker_objects():
     validate_yahoo_query_ticker_objects()
-    
