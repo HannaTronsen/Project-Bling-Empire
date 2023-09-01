d03d69f77e6aaa1bdb5703b9f809d0b1c7ad58de
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-09-01 10:34:18 +0200

    refactor const file

--- const.py
+++ const.py
@@ -1,18 +1,10 @@
-import re
-from context.ticker_scraper.main.const import *
-from context.yquery_ticker.main.const import *
-
-
-def _get_all_test_paths_list():
-    all_vars = globals().keys()
-    pattern = re.compile(".*TEST_PATH.*")
-    return [globals()[var] for var in all_vars if pattern.match(var)]
+from context.ticker_scraper.main.const import TICKER_SCRAPER_TEST_PATH
+from context.ticker_scraper.main.stock_collections import NORWAY
+from context.yquery_ticker.main.const import YQUERY_TEST_PATH
 
 
 class CONST_COLLECTION:
-    from context.ticker_scraper.main.stock_collections import (
-        NORWAY
-    )
+
     STCOK_COLLECTION_LIST = [
         # STANDARD_AND_POOR_500,
         NORWAY,
@@ -26,5 +18,8 @@ class CONST_COLLECTION:
 
 BLACKLISTED_STOCK_TICKERS_PATH = f'black_listed_stock_tickers.txt'
 AUTO_GENERATED_FILE_STRING = " ### This file is auto generated and shouldn't be touched! ### "
-TEST_PATHS = _get_all_test_paths_list()
+TEST_PATHS = [
+    TICKER_SCRAPER_TEST_PATH,
+    YQUERY_TEST_PATH
+]
 CONST_COLLECTION = CONST_COLLECTION()
