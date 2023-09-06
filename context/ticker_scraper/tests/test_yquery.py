5925bb1483eec502b741f178b478157c7167fab6
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-09-06 20:32:23 +0200

    refactor

--- context/ticker_scraper/tests/test_yquery.py
+++ context/ticker_scraper/tests/test_yquery.py
@@ -7,89 +7,38 @@ class test_yquery(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(test_yquery, self).__init__(*args, **kwargs)
         try:
-            self.error_prefix = "yquery.Ticker('MSFT')"
-            self.error_suffix = "caused and exception:"
+            self.error_message_prefix = "yquery.Ticker('MSFT')"
+            self.error_message_suffix = "caused and exception:"
             self.ticker = Ticker('MSFT')
+            self.ticker_fields = [
+                (self.ticker.summary_detail, "summary_detail"),
+                (self.ticker.summary_profile, "summary_profile"),
+                (self.ticker.history, "history"),
+                (self.ticker.dividend_history, "dividend_history"),
+                (self.ticker.history, "history"),
+                (self.ticker.income_statement, "income_statement"),
+                (self.ticker.balance_sheet, "balance_sheet"),
+                (self.ticker.cash_flow, "cash_flow"),
+                (self.ticker.major_holders, "major_holders"),
+                (self.ticker.insider_holders, "insider_holders"),
+                (self.ticker.earnings, "earnings"),
+                (self.ticker.recommendations, "recommendations"),
+                (self.ticker.news, "news")
+            ]
         except Exception as e:
             self.fail(
-                f'{self.error_prefix} in init {self.error_suffix} {e}. API might be down!'
+                f'{self.error_message_prefix} in init {self.error_message_suffix} {e}. API might be down!'
             )
         self.assertIsNone(None)
 
-    def test_summary(self):
-        try:
-            self.ticker.summary_detail
-            self.ticker.summary_profile
-        except Exception as e:
-            self.throw_generic_error("test_summary()", e)
-        self.assertIsNone(None)
-
-    def test_history(self):
-        try:
-            self.ticker.history()
-        except Exception as e:
-            self.throw_generic_error("test_history()", e)
-        self.assertIsNone(None)
-
-    def test_dividends(self):
-        try:
-            self.ticker.dividend_history,
-        except Exception as e:
-            self.throw_generic_error("test_dividends()", e)
-        self.assertIsNone(None)
-
-    def test_income_statement(self):
-        try:
-            self.ticker.income_statement
-        except Exception as e:
-            self.throw_generic_error("test_income_statement()", e)
-        self.assertIsNone(None)
-
-    def test_balance_sheet(self):
-        try:
-            self.ticker.balance_sheet
-        except Exception as e:
-            self.throw_generic_error("test_balance_sheet()", e)
-        self.assertIsNone(None)
-
-    def test_cash_flow(self):
-        try:
-            self.ticker.cash_flow
-        except Exception as e:
-            self.throw_generic_error("test_cash_flow()", e)
-        self.assertIsNone(None)
-
-    def test_holders(self):
-        try:
-            self.ticker.major_holders
-            self.ticker.insider_holders
-        except Exception as e:
-            self.throw_generic_error("test_holders()", e)
-        self.assertIsNone(None)
-
-    def test_earnings(self):
-
-        try:
-            self.ticker.earnings
-        except Exception as e:
-            self.throw_generic_error("test_earnings()", e)
-        self.assertIsNone(None)
-
-    def test_recommendations(self):
-        try:
-            self.ticker.recommendations
-        except Exception as e:
-            self.throw_generic_error("test_recommendations()", e)
-        self.assertIsNone(None)
-
-    def test_news(self):
-        try:
-            self.ticker.news
-        except Exception as e:
-            self.throw_generic_error("test_news()", e)
-        self.assertIsNone(None)
+    def test_ticker(self):
+        for field, field_name in self.ticker_fields:
+            try:
+                self.assertIsNotNone(field)
+            except Exception as e:
+                self.throw_generic_error(f"${field_name}()", e)
 
     def throw_generic_error(self, method_name, e):
         self.fail(
-            f'{self.error_prefix} {method_name} {self.error_suffix} {e}'
+            f'{self.error_message_prefix} {method_name} {self.error_message_suffix} {e}'
         )
