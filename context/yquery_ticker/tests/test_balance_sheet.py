f6cce0ad740f34014af6501f2214290274866205
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-09-26 18:54:09 +0200

    fixed tests

--- context/yquery_ticker/tests/test_balance_sheet.py
+++ context/yquery_ticker/tests/test_balance_sheet.py
@@ -1,11 +1,10 @@
 import unittest
-
 import pandas as pd
-
 from context.yquery_ticker.main.classes.balance_sheet_data import BalanceSheetData
-from context.yquery_ticker.main.classes.income_statement_data import IncomeStatementData
-from context.yquery_ticker.main.data_classes.date import Date
 from context.yquery_ticker.main.const import YQUERY_TEST_PATH
+from context.yquery_ticker.main.data_classes.date import Date
+from context.yquery_ticker.main.data_classes.yq_data_frame_data.balance_sheet import BalanceSheetDataClass
+from context.yquery_ticker.main.data_classes.yq_data_frame_data.yq_data_frame_data import PeriodType
 from context.yquery_ticker.main.enums.quarter import Quarter
 
 
@@ -14,86 +13,68 @@ class test_balance_sheet(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(test_balance_sheet, self).__init__(*args, **kwargs)
 
-        # self.annual_income_statement_expected_list = [
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2019, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=55256000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2020, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=57411000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2021, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=94680000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=99803000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.TTM,
-        #         netIncome=94760000000.0
-        #     )
-        # ]
-        #
-        # self.quarter_income_statement_expected_list = [
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=19442000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=20721000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.FOURTH_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=29998000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.FIRST_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=24160000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=19881000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.TTM,
-        #         netIncome=94760000000.0
-        #     )
-        # ]
+        self.annual_balance_sheet_expected_list = [
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2019, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                commonStockEquity=90488000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2020, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                commonStockEquity=65339000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2021, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                commonStockEquity=63090000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                commonStockEquity=50672000000.0
+            ),
+        ]
+
+        self.quarter_balance_sheet_expected_list = [
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.SECOND_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                commonStockEquity=58107000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                commonStockEquity=50672000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.FOURTH_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                commonStockEquity=56727000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2023, quarter=Quarter.FIRST_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                commonStockEquity=62158000000.0
+            ),
+            BalanceSheetDataClass(
+                asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                commonStockEquity=60274000000.0
+            ),
+        ]
 
     def test_convert_data_frame_to_model(self):
         json_file_name = "resources/balance_sheet/aapl.data.balance_sheet.annually.csv"
         data = f'{YQUERY_TEST_PATH}{json_file_name}'
 
-        BalanceSheetData.convert_data_frame_to_time_series_model(
+        assert BalanceSheetData.convert_data_frame_to_time_series_model(
             data_frame=pd.read_csv(data)
-        )
+        ) == self.annual_balance_sheet_expected_list
 
-    #     assert IncomeStatementData.convert_data_frame_to_time_series_model(
-    #         data_frame=pd.read_csv(data)
-    #     ) == self.annual_income_statement_expected_list
-    #
         json_file_name = "resources/balance_sheet/aapl.data.balance_sheet.quarterly.csv"
         data = f'{YQUERY_TEST_PATH}{json_file_name}'
 
-        BalanceSheetData.convert_data_frame_to_time_series_model(
+        assert BalanceSheetData.convert_data_frame_to_time_series_model(
             data_frame=pd.read_csv(data)
-        )
-    #
-    #     assert IncomeStatementData.convert_data_frame_to_time_series_model(
-    #         data_frame=pd.read_csv(data)
-    #     ) == self.quarter_income_statement_expected_list
+        ) == self.quarter_balance_sheet_expected_list
--- context/yquery_ticker/tests/test_cash_flow.py
+++ context/yquery_ticker/tests/test_cash_flow.py
@@ -1,8 +1,11 @@
 import unittest
-
 import pandas as pd
 from context.yquery_ticker.main.classes.cash_flow_data import CashFlowData
 from context.yquery_ticker.main.const import YQUERY_TEST_PATH
+from context.yquery_ticker.main.data_classes.date import Date
+from context.yquery_ticker.main.data_classes.yq_data_frame_data.cash_flow import CashFlowDataClass
+from context.yquery_ticker.main.data_classes.yq_data_frame_data.yq_data_frame_data import PeriodType
+from context.yquery_ticker.main.enums.quarter import Quarter
 
 
 class test_cash_flow(unittest.TestCase):
@@ -10,86 +13,80 @@ class test_cash_flow(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(test_cash_flow, self).__init__(*args, **kwargs)
 
-        # self.annual_income_statement_expected_list = [
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2019, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=55256000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2020, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=57411000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2021, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=94680000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_12,
-        #         netIncome=99803000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.TTM,
-        #         netIncome=94760000000.0
-        #     )
-        # ]
-        #
-        # self.quarter_income_statement_expected_list = [
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=19442000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=20721000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2022, quarter=Quarter.FOURTH_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=29998000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.FIRST_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=24160000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.MONTH_3,
-        #         netIncome=19881000000.0
-        #     ),
-        #     IncomeStatementDataClass(
-        #         asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
-        #         periodType=PeriodType.TTM,
-        #         netIncome=94760000000.0
-        #     )
-        # ]
+        self.annual_cash_flow_expected_list = [
+            CashFlowDataClass(
+                asOfDate=Date(year=2019, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                cashDividendsPaid=-14119000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2020, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                cashDividendsPaid=-14081000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2021, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                cashDividendsPaid=-14467000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_12,
+                cashDividendsPaid=-14841000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
+                periodType=PeriodType.TTM,
+                cashDividendsPaid=-14970000000.0
+            ),
+        ]
+
+        self.quarter_cash_flow_expected_list = [
+            CashFlowDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.SECOND_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                cashDividendsPaid=-3811000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.THIRD_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                cashDividendsPaid=-3703000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2022, quarter=Quarter.FOURTH_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                cashDividendsPaid=-3768000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2023, quarter=Quarter.FIRST_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                cashDividendsPaid=-3650000000.0
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
+                periodType=PeriodType.MONTH_3,
+                cashDividendsPaid=-3849000000.0
+
+            ),
+            CashFlowDataClass(
+                asOfDate=Date(year=2023, quarter=Quarter.SECOND_QUARTER),
+                periodType=PeriodType.TTM,
+                cashDividendsPaid=-14970000000.0
+
+            ),
+        ]
 
     def test_convert_data_frame_to_model(self):
         json_file_name = "resources/cash_flow/aapl.data.cash_flow.annually.csv"
         data = f'{YQUERY_TEST_PATH}{json_file_name}'
 
-        CashFlowData.convert_data_frame_to_time_series_model(
+        assert CashFlowData.convert_data_frame_to_time_series_model(
             data_frame=pd.read_csv(data)
-        )
+        ) == self.annual_cash_flow_expected_list
 
-    #     assert IncomeStatementData.convert_data_frame_to_time_series_model(
-    #         data_frame=pd.read_csv(data)
-    #     ) == self.annual_income_statement_expected_list
-    #
         json_file_name = "resources/cash_flow/aapl.data.cash_flow.quarterly.csv"
         data = f'{YQUERY_TEST_PATH}{json_file_name}'
 
-        CashFlowData.convert_data_frame_to_time_series_model(
+        assert CashFlowData.convert_data_frame_to_time_series_model(
             data_frame=pd.read_csv(data)
-        )
-    #
-    #     assert IncomeStatementData.convert_data_frame_to_time_series_model(
-    #         data_frame=pd.read_csv(data)
-    #     ) == self.quarter_income_statement_expected_list
+        ) == self.quarter_cash_flow_expected_list
