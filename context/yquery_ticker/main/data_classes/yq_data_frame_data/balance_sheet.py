643ae33d54b046219e02f2b6ca4d93dda697f0d7
Hanna Tronsen <hanna.tronsen@airthings.com>
2023-10-01 20:14:33 +0200

    Added iterable data interface back

--- context/yquery_ticker/main/data_classes/yq_data_frame_data/balance_sheet.py
+++ context/yquery_ticker/main/data_classes/yq_data_frame_data/balance_sheet.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from typing import Optional
+
+from context.yquery_ticker.main.classes.iterable_data import IterableDataInterface
 from context.yquery_ticker.main.data_classes.yq_data_frame_data.yq_data_frame_data import YQDataFrameData
 
 COMMON_STOCK_EQUITY = 'CommonStockEquity'
 
 
 @dataclass
-class BalanceSheetDataClass(YQDataFrameData):
+class BalanceSheetDataClass(IterableDataInterface, YQDataFrameData):
     commonStockEquity: Optional[float]
