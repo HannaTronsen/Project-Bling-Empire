477d87c3c8669569e6f6e9826de5f8077cc69006
Hanna Tronsen <hanna.tronsen@airthings.com>
2024-01-28 17:24:49 +0100

    re-generated csv for accurate dividend score

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TIME_STAMP = datetime.now().strftime("%Y-%m-%d")
 
 RUN_TESTS = True
 GENERATE_TICKER_CSV = True
-GENERATE_COMPARABLE_CSV = False
+GENERATE_COMPARABLE_CSV = True
 
 USE_OPTIMIZED_ALGORITHM = False
 
